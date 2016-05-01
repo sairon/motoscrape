900603afeaa6da6b481fdb2739bf567cfb10055b
Jan Čermák <jan.cermak@nic.cz>
2016-05-01 12:08:57 +0200

    reporter.sh: update arguments order in case of an error

--- reporter.sh
+++ reporter.sh
@@ -3,9 +3,9 @@
 # Wrapping script for reporting - usage: reporter.sh from to subject
 
 # Set PYTHONIOENCODING to avoid breaking of the pipes by Unicode chars
-PYTHONIOENCODING=UTF-8 ./run.sh 1>report.txt 2> ./err.txt
+PYTHONIOENCODING=UTF-8 ./run.sh 1>report.txt 2>err.txt
 if [ $? -eq 0 ]; then
     [ -s report.txt ] && ./sendreport.py -f $1 -t $2 -s $3 report.txt
 else
-    ./sendreport.py $1 $2 err.txt
+    ./sendreport.py -f $1 -t $2 -s $3 err.txt
 fi
