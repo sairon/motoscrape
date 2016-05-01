15bb975791f21fbb8c31fdc8927ee02c3fad0e63
Jan Čermák <jan.cermak@nic.cz>
2016-05-01 12:16:29 +0200

    reporter.sh: add forgotten quotes around arguments

--- reporter.sh
+++ reporter.sh
@@ -5,7 +5,7 @@
 # Set PYTHONIOENCODING to avoid breaking of the pipes by Unicode chars
 PYTHONIOENCODING=UTF-8 ./run.sh 1>report.txt 2>err.txt
 if [ $? -eq 0 ]; then
-    [ -s report.txt ] && ./sendreport.py -f $1 -t $2 -s $3 report.txt
+    [ -s report.txt ] && ./sendreport.py -f "$1" -t "$2" -s "$3" report.txt
 else
-    ./sendreport.py -f $1 -t $2 -s $3 err.txt
+    ./sendreport.py -f "$1" -t "$2" -s "$3" err.txt
 fi
